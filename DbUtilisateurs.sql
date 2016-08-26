fe1aa682ea4594503b1e6bd660f21cd21f0dfbc3
fayajay <jeremybecker5906@gmail.com>
2016-08-26 10:09:39 +0200

    version sql

---
+++ DbUtilisateurs.sql
@@ -0,0 +1,10 @@
+INSERT INTO Conducteur (id,loginConducteur,mdpConducteur,mailConducteur,telConducteur,immatriculation,nbPlaces,latitude,longitude)
+VALUES (1,'seb','seb','seb@seb.fr','0600010203','EH-789-PQ',2,50.6,3.1),
+       (2,'jay','jay','jay@jay.fr','0708090405','AB-456-BG',7,50.3,3.5),
+       (3,'sab','sab','sab@sab.fr','0801020603','FS-123-CG',1,50.9,3.3);
+
+INSERT INTO Passager (id,loginPassager,mdpPassager,mailPassager,telPassager)
+VALUES (1,'Kevin','beauf','kevin@kevin.fr','01020304'),
+       (2,'Brian','bobo','brian@brian.fr','0605040302'),
+       (3,'Dylan','keke','dylan@dylan.fr','05010203');
+
