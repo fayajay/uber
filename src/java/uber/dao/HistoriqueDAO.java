c7eef2dd769f0f91ec6b02bb3465414af5c6b142
Login_GITHUB <cherg@DESKTOP-7F79JFU>
2016-08-29 14:59:25 +0200

    HistoriqueDAO

---
+++ src/java/uber/dao/HistoriqueDAO.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uber.dao;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+import uber.entity.Historique;
+
+/**
+ *
+ * @author cherg
+ */
+public class HistoriqueDAO {
+        public List<Historique> lister() {
+    EntityManager em = Persistence.createEntityManagerFactory("PU").createEntityManager();
+    return em.createQuery("SELECT h FROM Historique h").getResultList();
+}
+    
+}
